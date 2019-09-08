07f35c9c98effec9e762d7f898c7e00edf9f5fa5
hiroki19990625 <hiroki19990625@gmail.com>
2019-09-08 16:21:47 +0900

    Ackの実装

--- UdpNetworking/ReliabilityUdpClientSession.cs
+++ UdpNetworking/ReliabilityUdpClientSession.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Net;
 using UdpNetworking.Packet.ConnectedPacket;
 using UdpNetworking.Packet.HighLevel;
@@ -12,6 +14,8 @@ namespace UdpNetworking
         public IPEndPoint EndPoint { get; }
         public ushort MtuSize { get; }
 
+        public Dictionary<uint, DataPacket> ReSendPackets = new Dictionary<uint, DataPacket>();
+
         public uint SendSequenceId { get; private set; }
         public uint SendMessageId { get; private set; }
         public ushort SendSplitId { get; private set; }
@@ -25,14 +29,31 @@ namespace UdpNetworking
 
         public void OnReceive(DataPacket dataPacket)
         {
+            AckPacket packet = new AckPacket(new[] {dataPacket.SequenceId});
+            _client.Send(EndPoint, packet);
         }
 
         public void OnAck(AckPacket ackPacket)
         {
+            for (int i = 0; i < ackPacket.SequenceIds.Count; i++)
+            {
+                if (ReSendPackets.ContainsKey(ackPacket.SequenceIds[i]))
+                {
+                    ReSendPackets.Remove(ackPacket.SequenceIds[i]);
+                    Console.WriteLine($"[{EndPoint}] Ack: {ackPacket.SequenceIds[i]}");
+                }
+            }
         }
 
         public void OnNack(NackPacket nackPacket)
         {
+            for (int i = 0; i < nackPacket.SequenceIds.Count; i++)
+            {
+                if (ReSendPackets.ContainsKey(nackPacket.SequenceIds[i]))
+                {
+                    SendDataPacket(ReSendPackets[nackPacket.SequenceIds[i]].Data);
+                }
+            }
         }
 
         public void SendPacket(HighLevelPacket packet)
@@ -48,6 +69,7 @@ namespace UdpNetworking
         public void SendDataPacket(byte[] buf)
         {
             DataPacket dataPacket = new DataPacket(SendSequenceId++, buf);
+            ReSendPackets.Add(dataPacket.SequenceId, dataPacket);
             _client.Send(EndPoint, dataPacket);
         }
     }
